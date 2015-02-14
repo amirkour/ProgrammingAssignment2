29e0c70006ea10ad728ca843230d44a3a978d598
amir kouretchian <amirsgarbunk@gmail.com>
2015-02-14 12:46:49 -0800

    implementation for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## cachematrix.R
+##
+## constructor and helper for initializing and caching the inverse
+## of invertible matrices.
+##
 
-## Write a short comment describing this function
-
+##
+## makeCacheMatrix
+##
+## constructor for "cache matrix" objects.
+## these objects simply cache the inverse of the given matrix x,
+## so that you don't have to calculate that inverse over and over
+## at runtime.  this function assumes that x is incertible, and so
+## no checking is performed to that end.
+##
+## note that the initial value of the inverse of x has to be calculated
+## with the helper function cacheSolve.
+##
+## for example:
+## x <- someCoolMatrix
+## cacheOfXInverse <- makeCacheMatrix(x)
+## cacheSolve(cacheOfXInverse)
+##
+## now you can get the inverse of x:
+## cacheOfXInverse$getinverse()
+##
 makeCacheMatrix <- function(x = matrix()) {
+    cachedinverse <- NULL
 
+    getinverse <- function() cachedinverse
+    setinverse <- function(newinverse) cachedinverse <<- newinverse
+    get <- function() x
+    set <- function(newmatrix){
+        x <<- newmatrix
+        cachedinverse <<- NULL
+    }
+
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+##
+## cacheSolve
+##
+## helper function that calculates and stores the inverse of the given
+## "cache matrix" object.  use the constructor function makeCacheMatrix
+## to construct such an object.
+##
+## note that this function assumes the matrix for oCacheMatrix is invertible,
+## and will throw runtime errors if that is not the case.
+##
+cacheSolve <- function(oCacheMatrix, ...) {
+    ## Return a matrix that is the inverse of 'oCacheMatrix'
+    inverse <- oCacheMatrix$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached inverse")
+        return(inverse)
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    data <- oCacheMatrix$get()
+    inverse <- solve(data,...)
+    oCacheMatrix$setinverse(inverse)
+    inverse
 }
